e93ddcc14bdcc99871566666bbe7345ffa3f08a0
Baidewei <gerant9seminario@gmail.com>
2024-02-20 21:28:49 -0400

    aaAAAAAA

--- client/src/components/landing/LandingPage.jsx
+++ client/src/components/landing/LandingPage.jsx
@@ -2,18 +2,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './LandingPage.css';
+import LandingEarth from '../../images/landingearth.png'
 
 export default function LandingPage({ handleAccess }) {
   return (
     <div>
       <div className="earth-container">
         <Link to="/loading">
-          <img className='earth' src='https://countriesgerant.netlify.app/client/src/images/landingearth.png' alt="Home" onClick={handleAccess} style={{ cursor: 'pointer' }}></img>
-        </Link>
-    </div>
-        <Link to="/loading">
-          <a onClick={handleAccess}>Home</a>
+          <img className='earth' src={LandingEarth} alt="Home" onClick={handleAccess} style={{ cursor: 'pointer' }}></img>
         </Link>
       </div>
+    </div>
   );
 }
